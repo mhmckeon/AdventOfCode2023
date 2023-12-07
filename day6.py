7b27e3145e3072cb33492b4e0defa2a33fc4ae55
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-12-07 17:16:29 +0000

    more efficient algorithm

--- day6.py
+++ day6.py
@@ -30,24 +30,53 @@ for x in range(len(times)):
 
 print(total)
 
-# Part 2
-
+# Part 2 initial 
+import time
+start = time.time()
 with open(input) as f:
     lines = f.read().split("\n")
 
 
-times = []
-distance = []
 for index, line in enumerate(lines):
     lines[index] = line.split(":")[1].split()
 
-time = int("".join(lines[0]))
+newtime = int("".join(lines[0]))
 distance = int("".join(lines[1]))
-print(time, distance)
+print(newtime, distance)
 count = 0
-for mili in range(time):
-    if mili * (time-mili) > distance:
+for mili in range(newtime):
+    if mili * (newtime-mili) > distance:
         count+=1
 print(count)
 print("finished!")
 
+stop = time.time()
+print("Day 2 un-optimized",stop-start)
+
+# Part 2 optimised
+start = time.time()
+
+with open(input) as f:
+    lines = f.read().split("\n")
+
+for index, line in enumerate(lines):
+    lines[index] = line.split(":")[1].split()
+
+newtime = int("".join(lines[0]))
+distance = int("".join(lines[1]))
+newtime2 = round(newtime / 2)
+print(newtime)
+print(newtime2)
+
+print(newtime2, distance)
+
+count = 0
+for mili in range(newtime2+1):
+    if mili * (newtime-mili) > distance:
+        count+=1
+
+print(count*2-1)
+print("finished!")
+
+stop = time.time()
+print("Day 2 optimised:", stop-start)
