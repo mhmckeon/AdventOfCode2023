e76e7e0420216dbf519acb121f770889ddb4c167
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-12-09 18:55:15 +0000

    day9 updated

--- day9.py
+++ day9.py
@@ -6,10 +6,10 @@ with open(input) as f:
 
 total = 0
 for line in lines:
-    line = line.split(" ")
-    eol, tempList = [int(line[-1])], None
+    line = list(map(int, line.split()))
+    eol, tempList = [line[-1]], None
     while tempList == None or len(tempList) != tempList.count(0):
-        tempList = [int(line[x])-int(line[x-1]) for x in range(1,len(line))]
+        tempList = [line[x]-line[x-1] for x in range(1,len(line))]
         eol.append(tempList[-1])
         line = tempList
     total += (sum(eol))
@@ -19,10 +19,10 @@ print("Part 1:", total)
 # Part 2
 total = 0
 for line in lines:
-    line = line.split(" ")[::-1]
-    eol, tempList = [int(line[-1])], None
-    while tempList == None or len(tempList) != tempList.count(0):
-        tempList = [int(line[x])-int(line[x-1]) for x in range(1,len(line))]
+    line = list(map(int,line.split()))[::-1]
+    eol, tempList = [line[-1]], None
+    while not all(x==0 for x in line):
+        tempList = [line[x]-line[x-1] for x in range(1,len(line))]
         eol.append(tempList[-1])
         line = tempList
     total += (sum(eol))
