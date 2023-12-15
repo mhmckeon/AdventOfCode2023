78d63b13141a5c85a87d0aea027105daa9aa2f6a
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-12-15 15:20:01 +0000

    P2 still wrong, need to restart it

--- day13.py
+++ day13.py
@@ -58,45 +58,67 @@ def patternCheck2(pattern):
                 return x
     return False
                    
-total = 0
-for pattern in patterns:
+rowTotal = 0
+colTotal = 0
+for num, pattern in enumerate(patterns):
     found = False
+    row = 0
+    column = 0
     while not found:
-        # change one symbol
-        # # # # # # # # # #
-        found = patternCheck2(pattern)
-        
+        patt = pattern.copy()
+        if patt[column][row] == ".":
+            patt[column] = patt[column][:row] + "#" + patt[column][row+1:]
+            print(123)
+        else:
+            print(patt)
+            patt[column]=patt[column][:row] + "." + patt[column][row+1:]
+            print(patt)
+        row+=1
+        if row == len(patt[column]):
+            row = 0
+            column += 1
+        if column == len(patt):
+            if patternCheck2(pattern):
+                rowTotal += patternCheck2(pattern) 
+            else:
+                cols = list(zip(*patt))
+                for newind, col in enumerate(cols):
+                    cols[newind] = "".join(col)
+                found = patternCheck2(cols)
+                colTotal += found
+
+        found = patternCheck2(patt)
+        if found:
+            if num in rowsDict:
+                if rowsDict[num] == found:
+                    found = False
+                else:
+                    rowTotal += found
+            else:
+                rowTotal += found
+        print(1,rowTotal)
         if not found:
-            cols = list(zip(*pattern))
+            cols = list(zip(*patt))
             for newind, col in enumerate(cols):
                 cols[newind] = "".join(col)
             columns.append(cols)
             found = patternCheck2(cols)
+            if found:
+                if num in colsDict:
+                    if colsDict[num] == found:
+                        found = False
+                    else:
+                        colTotal += found
+                else:
+                    colTotal += 1
 
-    total += found
-        
+print(rowTotal)
+print(colTotal)
 
 
-totalRows =patternCheck2(patterns)
-columns = []
-for pattern in patterns:
-    cols = list(zip(*pattern))
-    for newind, col in enumerate(cols):
-        cols[newind] = "".join(col)
-    columns.append(cols)
 
-totalCols = patternCheck2(columns, colsDict)
-print(totalCols)
-print(totalRows)
-
-overall = (totalRows*100) + totalCols
-print(overall) 
 
 # 1500 too low
 # 44813 too high
 
 
-
-print(list(rowsDict.keys()))
-
-print(list(colsDict.keys()))
