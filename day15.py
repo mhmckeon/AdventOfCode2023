421f5dd863fb6fe54aafdc44774e4c17f48b1ec5
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-12-15 12:45:10 +0000

    Day 15 Completed

--- day15.py
+++ day15.py
@@ -1,17 +1,54 @@
 # Day 15
+# Part1
 
-words =  open("input/day15.txt").read().split(",")
+words = open("input/day15.txt").read().split(",")
 
 
-def hashChar(char, value):
-    return ((ord(char)+value)*17)%256
+def hashWord(char):
+    value=0
+    for x in char:
+        value = ((ord(x)+value)*17)%256
+    return value
 
 total = 0
 for word in words:
-    value=0
-    for x in word:
-        value = hashChar(x, value)
-    total += value
+    total += hashWord(word)
 
-print(value)
-print(total)
+print("Part 1:",total)
+
+dictBoxes = {}
+# Part 2
+for word in words:
+    if word.count("=") == 1:
+        duo = (word.split("="))
+        box = hashWord(duo[0])
+        if box in dictBoxes.keys():
+            added = False
+            for x in dictBoxes:
+                for ind, lense in enumerate(dictBoxes[x]):
+                    if lense[0] == duo[0]:
+                        dictBoxes[x][ind] = duo
+                        added = True
+                        break
+            if not added:
+                dictBoxes[box] += [duo]
+        else:
+            dictBoxes[box] = [duo]
+                         
+    else:
+        duo = (word.split("-"))
+        box = hashWord(duo[0])
+        for x in dictBoxes:
+            for ind, lense in enumerate(dictBoxes[x]):
+                if lense[0] == duo[0]:
+                    dictBoxes[x].remove(dictBoxes[x][ind])
+
+
+total = 0
+for box in dictBoxes:
+    num = box + 1
+    for ind, lense in enumerate(dictBoxes[box]):
+        lense = int(lense[1])
+        total += num * (1 + ind) * lense
+
+print("Part 2:",total)
