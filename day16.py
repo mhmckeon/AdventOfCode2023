86b5747807c9917c63354d98fd4ff0da9b55a7d8
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-12-16 22:23:48 +0000

    mostly done

--- day16.py
+++ day16.py
@@ -2,10 +2,12 @@
 # Part 1
 input = "input/day16.txt"
 with open(input) as f:
-    lines = f.read().split("\n")
+    GRID = f.read().split("\n")
 
+GRIDLENGTH = len(GRID)
+GRIDWIDTH = len(GRID[0])
 
-class tile:
+class Tile:
     totalTouched = 0
 
     def __init__(self, tileType):
@@ -17,35 +19,163 @@ class tile:
 
     def lightUp(self):
         if self.touched == False:
-            tile.totalTouched += 1
+            Tile.totalTouched += 1
             self.touched = True 
+    
+    def __repr__(self):
+        return self.tileType
     
 
 
-class beam:
+class Beam:
     def __init__(self, start, direction):
+        """_summary_ moves along in direction from start coordinates
+
+        Args:
+            start (list): two integers in a list 
+            direction (string): "north", "south", "east", "west"
+        """
         self.start = start
         self.position = start
         self.direction = direction
         self.finished = False
+        GRID[self.position[1]][self.position[0]].lightUp() # lights up start tile
 
-    def move(self, grid):
+    def newBeams(self, tileType):
+        beam1, beam2 = None, None
+        # splitters
+        if tileType == "|":
+            if self.position[1]+ 1 <= GRIDLENGTH:
+                beam1 = Beam([self.position[0],self.position[1]+1], "north")
+            if self.position[1] - 1 >= 0:
+                beam2 = Beam([self.position[0],self.position[1]-1], "south")
+        elif tileType == "-":
+            if self.position[0]+ 1 <= GRIDWIDTH:
+                beam1 = Beam([self.position[0]+1,self.position[1]], "east")
+            if self.position[0] - 1 >= 0:
+                beam2 = Beam([self.position[0]-1,self.position[1]], "west")
+        # mirrors \\
+        elif tileType == "\\" and self.direction == "east":
+            if self.position[1] - 1 >= 0:
+                beam2 = Beam([self.position[0],self.position[1]-1], "south")
+        elif tileType == "\\" and self.direction == "west":
+            if self.position[1]+ 1 <= GRIDLENGTH:
+                beam1 = Beam([self.position[0],self.position[1]+1], "north")
+        elif tileType == "\\" and self.direction == "north":
+            if self.position[0] - 1 >= 0:
+                beam2 = Beam([self.position[0]-1,self.position[1]], "west")
+        elif tileType == "\\" and self.direction == "south":
+            if self.position[0]+ 1 <= GRIDWIDTH:
+                beam1 = Beam([self.position[0]+1,self.position[1]], "east")
+        # mirrors / 
+        elif tileType == "/" and self.direction == "east":
+            if self.position[1]+ 1 <= GRIDLENGTH:
+                beam1 = Beam([self.position[0],self.position[1]+1], "north")
+        elif tileType == "/" and self.direction == "west":
+            if self.position[1] - 1 >= 0:
+                beam2 = Beam([self.position[0],self.position[1]-1], "south")   
+        elif tileType == "/" and self.direction == "south":
+            if self.position[0] - 1 >= 0:
+                beam2 = Beam([self.position[0]-1,self.position[1]], "west")
+        elif tileType == "/" and self.direction == "north":
+            if self.position[0]+ 1 <= GRIDWIDTH:
+                beam1 = Beam([self.position[0]+1,self.position[1]], "east")          
+
+        if all(item is not None for item in [beam1, beam2]):
+            return [beam1, beam2]
+        elif beam1 != None:
+            return [beam1]
+        elif beam2 != None:
+            return [beam2]
+        else:
+            print("no more tiles this way")
+            return True
+
+
+    def move(self):
+        currentTile = GRID[self.position[1]][self.position[0]]
         if self.direction == "north":
-            self.position = (self.position[0],self.position[1]+1)
-            if self.position[1] >= len(grid): # change to just greater than if issues
-                self.finished = True
+            while True:
+                self.position = (self.position[0],self.position[1]+1)
+                if self.position[1] >= GRIDLENGTH: # change to just greater than if issues
+                    self.finished = True
+                    return True
+                currentTile = GRID[self.position[1]][self.position[0]]
+                currentTile.lightUp(self.position[1], self.position[0])
+                if currentTile.tileType in ["-", "/","\\"]:
+                    self.finished = True
+                    currentTile.directionsDone[self.direction] = True
+                    currentTile.directionsDone["south"] = True
+                    return self.position
             
         elif self.direction == "south":
-            self.position = (self.position[0],self.position[1]-1)
-            if self.position[1] < 0:
-                self.finished = True
+            while True:
+                self.position = (self.position[0],self.position[1]-1)
+                if self.position[1] < 0:
+                    self.finished = True
+                    return True
+                currentTile = GRID[self.position[1]][self.position[0]]
+                currentTile.lightUp(self.position[1], self.position[0])
+                if currentTile.tileType == "-":
+                    self.finished = True
+                    currentTile.directionsDone[self.direction] = True
+                    currentTile.directionsDone["north"] = True
+                    return self.position
 
         elif self.direction == "east":
-            self.position = (self.position[0] + 1,self.position[1])
-            if self.position[0] >= len(grid[0]):
-                self.finished = True
-                
+            while True:
+                self.position = (self.position[0] + 1,self.position[1])
+                if self.position[0] >= GRIDWIDTH:
+                    self.finished = True
+                    return True
+                currentTile = GRID[self.position[1]][self.position[0]]
+                currentTile.lightUp()
+                if currentTile.tileType == "|":
+                    self.finished = True
+                    currentTile.directionsDone[self.direction] = True
+                    currentTile.directionsDone["west"] = True
+                    return self.newBeams(currentTile.tileType)
+
         elif self.direction == "west":
-            self.position = (self.position[0] - 1,self.position[1])
-            if self.position[0] < 0:
-                self.finished = True
+            while True:
+                self.position = (self.position[0] - 1,self.position[1])
+                if self.position[0] < 0:
+                    self.finished = True
+                    return True
+                currentTile = GRID[self.position[1]][self.position[0]]
+                currentTile.lightUp()
+                if currentTile.tileType == "|":
+                    self.finished = True
+                    currentTile.directionsDone[self.direction] = True
+                    currentTile.directionsDone["east"] = True
+                    return self.newBeams()
+
+
+
+for indY, down in enumerate(GRID):
+    tempRight = []
+    for right in down:
+        tempRight.append(Tile(right))
+    GRID[indY] = tempRight
+
+print(GRID[0][1])
+
+
+ogBeamer = Beam([0,0],"east")
+currentBeams = [ogBeamer]
+
+# while currentBeams: 
+tempBeams = []
+for beam in currentBeams:
+    movement = beam.move()
+    if movement != True:
+        tempBeams.append(*movement)
+
+    currentBeams = tempBeams.copy()
+
+print(currentBeams)
+
+print(Tile.totalTouched)
+
+
+# need to add both backslash beams 
